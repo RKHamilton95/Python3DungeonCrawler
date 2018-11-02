41dfdfe129279a64d3a9e998127bbba66f6303b6
Ryan Hamilton <ryan.k.hamilton@wmich.edu>
2018-11-02 13:02:45 -0400

    Player Added Inventory

--- Character Classes/Character.py
+++ Character Classes/Character.py
@@ -1,31 +1,56 @@
 class Player(object):
+    
     def __init__(self):
         self.playerName = ""
         self.playerHealth = int()  
         self.playerDamage = int()
-        self.playerWeaponDamage = int()
         self.playerWeaponName = ""
-    
+        self.inventory = Inventory()
+
+    #Getters
     def getPlayerHealth(self):
         return self.playerHealth
+
+    #def playerGrabsWeapon(self,weaponName):
+    #    self.playerWeaponName = weaponName
+    #    self.playerDamage = self.weaponDict[weaponName]
     
-    def setPlayerHealth(self,damage):
+    def playerTakesDamage(self,damage):
         self.playerHealth = self.playerHealth - damage
     
-class Elf(Player):
+    def playerHeals(self,amountHealed):
+        if((self.playerHealth+amountHealed) >= 100):
+            self.playerHealth = 100
+        else:
+            self.playerHealth = self.playerHealth+amountHealed
+    
+class Inventory(Player):
+    
+    weaponDict = {"Sword":5,"Axe": 7,"Mace":8,"Claymore":10}
+    healthPotionDict = {"Healing": 10,"Greater Healing":20,"Superior Healing":50,"FulL Healing":100}
+
     def __init__(self):
-        self.playerName = ""
-        self.playerHealth = 80
-        self.playerDamage = 5
-        self.playerWeaponDamage = 0
-        self.playerWeaponName = ""
+        self.inventory = dict()
+
+    def addItemToInventory(self,ItemName):
+        if ItemName not in self.inventory:
+            self.inventory.update({ItemName:1})
+        else:
+            self.inventory[ItemName] += 1
+    
+    def printInventory(self):
+        print(self.inventory)
+
+#class Elf(Player):
+    #def __init__(self):
+    #    self.playerName = ""
+    #    self.playerHealth = 80
+    #    self.playerDamage = 5
+    #    self.playerWeaponDamage = 0
+    #    self.playerWeaponName = ""
 
 Player1 = Player()
-Player1.playerName = "DONNIE"
-#print(Player1.playerName)
-ElfPlayer = Elf()
-ElfPlayer.playerName = "DILLY"
-print(ElfPlayer.getPlayerHealth())
-ElfPlayer.setPlayerHealth(10)
-print(ElfPlayer.getPlayerHealth())
-print(ElfPlayer.playerName)
+Player1.inventory.addItemToInventory("Sword")
+Player1.inventory.addItemToInventory("Healing")
+Player1.inventory.printInventory()
+
