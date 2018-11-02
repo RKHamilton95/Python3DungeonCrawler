96f9f31c7105fad86be372cb33ee3d6d0851f479
Ryan Hamilton <ryan.k.hamilton@wmich.edu>
2018-11-02 13:47:46 -0400

    Changes to Character class and Enemy class
    Heal Player needs to check if the item is in the inventory if not don't heal. Inventory needs some more work and same with the Character class

--- Character Classes/Character.py
+++ Character Classes/Character.py
@@ -1,9 +1,24 @@
+youDiedASCII = """\
+          _______             ______  _________ _______  ______    
+|\     /|(  ___  )|\     /|  (  __  \ \__   __/(  ____ \(  __  \   
+( \   / )| (   ) || )   ( |  | (  \  )   ) (   | (    \/| (  \  )  
+ \ (_) / | |   | || |   | |  | |   ) |   | |   | (__    | |   ) |  
+  \   /  | |   | || |   | |  | |   | |   | |   |  __)   | |   | |  
+   ) (   | |   | || |   | |  | |   ) |   | |   | (      | |   ) |  
+   | |   | (___) || (___) |  | (__/  )___) (___| (____/\| (__/  )  
+   \_/   (_______)(_______)  (______/ \_______/(_______/(______/   
+"""
+
+
 class Player(object):
     
+    weaponDict = {"Sword":5,"Axe": 7,"Mace":8,"Claymore":10}
+    healthPotionDict = {"Healing": 10,"Greater Healing":20,"Superior Healing":50,"FulL Healing":100}
+
     def __init__(self):
         self.playerName = ""
-        self.playerHealth = int()  
-        self.playerDamage = int()
+        self.playerHealth = 100  
+        self.playerDamage = 5
         self.playerWeaponName = ""
         self.inventory = Inventory()
 
@@ -11,24 +26,42 @@ class Player(object):
     def getPlayerHealth(self):
         return self.playerHealth
 
-    #def playerGrabsWeapon(self,weaponName):
-    #    self.playerWeaponName = weaponName
-    #    self.playerDamage = self.weaponDict[weaponName]
+    def playerSwitchesToWeapon(self,weaponName):
+        self.playerWeaponName = weaponName
+        self.playerDamage = self.weaponDict[weaponName]
     
     def playerTakesDamage(self,damage):
-        self.playerHealth = self.playerHealth - damage
+        if self.playerHealth - damage == 0:
+            print(youDiedASCII)
+            exit(0)
+        else:
+            self.playerHealth = self.playerHealth - damage
+
+    def playerDoesDamage(self):
+        return self.playerDamage
     
     def playerHeals(self,amountHealed):
         if((self.playerHealth+amountHealed) >= 100):
             self.playerHealth = 100
         else:
             self.playerHealth = self.playerHealth+amountHealed
-    
+
+class Enemy(object):
+
+    def __init__(self):
+        self.enemyName = ""
+        self.enemyHealth = 30
+        self.enemyDamage = 5
+
+    def damageEnemy(self,damageDone):
+        self.enemyHealth -= damageDone
+
+    def doDamage(self):
+        return self.enemyDamage
+
+
 class Inventory(Player):
     
-    weaponDict = {"Sword":5,"Axe": 7,"Mace":8,"Claymore":10}
-    healthPotionDict = {"Healing": 10,"Greater Healing":20,"Superior Healing":50,"FulL Healing":100}
-
     def __init__(self):
         self.inventory = dict()
 
@@ -38,19 +71,32 @@ class Inventory(Player):
         else:
             self.inventory[ItemName] += 1
     
+    def removeFromInventory(self,ItemName):
+        if ItemName in self.inventory:
+            if self.inventory[ItemName] == 1:
+                self.inventory.pop(ItemName)
+            else:
+                self.inventory[ItemName] -= 1
+        else:
+            print(ItemName,"Is not in inventory")
+    
     def printInventory(self):
         print(self.inventory)
 
-#class Elf(Player):
-    #def __init__(self):
-    #    self.playerName = ""
-    #    self.playerHealth = 80
-    #    self.playerDamage = 5
-    #    self.playerWeaponDamage = 0
-    #    self.playerWeaponName = ""
 
 Player1 = Player()
-Player1.inventory.addItemToInventory("Sword")
-Player1.inventory.addItemToInventory("Healing")
-Player1.inventory.printInventory()
+#Damage Test
+#Player1.playerTakesDamage(100)
+
+#Inventory Tests
+#Player1.inventory.addItemToInventory("Sword")
+#Player1.inventory.addItemToInventory("Sword")
+#Player1.inventory.addItemToInventory("Sword")
+#Player1.inventory.addItemToInventory("Sword")
+#Player1.inventory.addItemToInventory("Healing")
+#Player1.inventory.printInventory()
+#Player1.inventory.removeFromInventory("Sword")
+#Player1.inventory.removeFromInventory("Sword")
+#Player1.inventory.removeFromInventory("Cat")
+#Player1.inventory.printInventory()
 
